417cc544ee0731e52461b31e8943bad65aa217b7
BiagioAntonelli <antonelli.bi@gmail.com>
2016-05-01 21:47:13 +0200

    Added files via upload

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Setting wd
+dir<-setwd("~/Desktop/exploratory data analysis")
+
+# reading the data
+dataset<-read.csv2("household_power_consumption.txt" ,  stringsAsFactors = FALSE)
+x<-as.POSIXct(paste(dataset$Date, dataset$Time), format="%d/%m/%Y %H:%M:%S")
+library(dplyr)
+dataset<-mutate(dataset,x)
+dataset$Date<-as.Date(dataset$Date,"%d/%m/%Y")
+
+
+#Subsetting  from 2007-02-01 to 2007-02-02
+limits<-which(dataset$Date=="2007-02-01" |dataset$Date=="2007-02-02") 
+subset<-dataset[min(limits):max(limits),]
+
+#Plot 2
+plot(subset$x,subset$ Global_active_power,type="l", xlab=paste(""),ylab=paste("Global_active_power"))
+
+dev.copy(png, file = "plot2.png")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,24 @@
+# Setting wd
+dir<-setwd("~/Desktop/exploratory data analysis")
+
+# reading the data
+dataset<-read.csv2("household_power_consumption.txt" ,  stringsAsFactors = FALSE)
+x<-as.POSIXct(paste(dataset$Date, dataset$Time), format="%d/%m/%Y %H:%M:%S")
+library(dplyr)
+dataset<-mutate(dataset,x)
+dataset$Date<-as.Date(dataset$Date,"%d/%m/%Y")
+
+
+#Subsetting  from 2007-02-01 to 2007-02-02
+limits<-which(dataset$Date=="2007-02-01" |dataset$Date=="2007-02-02") 
+subset<-dataset[min(limits):max(limits),]
+
+#plot 3
+with(subset, plot(x, Sub_metering_1 ,type="l", ylab="Energy sub metering"))
+with(subset, points(x, Sub_metering_2 ,type="l", col="red"))
+with(subset, points(x, Sub_metering_3 ,type="l", col="blue"))
+legend("topright", pch = 1, col = c("black","blue","red"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+
+dev.copy(png, file = "plot3.png")
+dev.off()
+
